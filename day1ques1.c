3faad57bad8c57ba7ef8095be493f4492bf6d525
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-08-16 20:35:04 +0530

    Add files via upload
    day-1
    ques-1
    coding procedure

---
+++ day1ques1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+//Q1: Write a program to input two numbers and display their sum.
+int a,b;
+printf("enter a");
+scanf("%d", & a);
+printf("enter b");
+scanf("%d", & b);
+int sum= a+b;
+printf("sum is :%d\n", sum);
+return 0;
+}
