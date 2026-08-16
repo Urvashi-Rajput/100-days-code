bdd60c7d45a5a1fdef1483a1efd9150bff2c99ec
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-08-16 20:28:24 +0530

    Add files via upload
    day-2
    ques-3
    coding

---
+++ day2ques3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int l,b;
+printf("enter l");
+scanf("%d", & l);
+printf("enter b");
+scanf("%d", & b);
+printf("perimeter is %d\n", 2*(l+b));
+printf("area is %d\n", l*b);
+return 0;
+}
