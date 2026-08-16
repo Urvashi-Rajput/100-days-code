627a2c2ab2a555a3a34f91cc5c9af0eb4e6ffb0a
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-08-16 20:16:17 +0530

    Add files via upload
    day-1
    ques-2
    coding

---
+++ day1ques2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int a,b;
+printf("enter a");
+scanf("%d", & a);
+printf("enter b");
+scanf("%d", & b);
+int sum= a+b;
+printf("sum is :%d\n", sum);
+int quotient= a/b;
+printf("quotient is :%d\n", quotient);
+int product=a*b;
+printf("product is :%d\n", product);
+int difference =a-b;
+printf("difference is :%d\n", difference);
+return 0;
+}
