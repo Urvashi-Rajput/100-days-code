c8aa401d7813dc288bfd0a5f23f57dfe1821bc8f
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-08-17 10:29:02 +0530

    Add files via upload
    day-3
    ques-5
    coding procedure

---
+++ day3ques5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+int celcius, fahrenheit;
+printf("enter celcius");
+scanf("%d", &celcius);
+printf("convergen to fahrenheit %d\n", (celcius*9/5)+32);
+return 0;
+}
