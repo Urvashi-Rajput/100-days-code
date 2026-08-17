6a74d8bc6821c2c9cb1dfa8d05e0a4245751b6ee
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-08-17 12:12:04 +0530

    Add files via upload
    day-6
    ques-11
    coding procedure

---
+++ day6ques11.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+//Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+int n;
+printf("enter n");
+scanf("%d", &n);
+if(n%2==0)
+{
+printf("even\n");
+}
+else
+{
+printf("odd\n");
+}
+return 0;
+}
+
