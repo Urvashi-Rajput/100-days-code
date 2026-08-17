2919361aaf90db8434e1938d23d88cb373a75c58
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-08-17 11:05:56 +0530

    Add files via upload
    day-4
    ques-8
    coding procedure

---
+++ day4ques8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+//Q8: Write a program to find and display the sum of the first n natural numbers.
+int n,sum;
+printf("enter n");
+scanf("%d", &n);
+printf("sum is %d\n", n*(n+1)/2);
+return 0;
+}
+
