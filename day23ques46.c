426c3b95dc726f93cf3d13b2f8250f8d43c677e6
Urvashi-Rajput <urvashi.38714@stu.upes.ac.in>
2026-09-05 16:28:37 +0530

    Add files via upload
    day-23
    ques-46
    coding procedure

---
+++ day23ques46.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+//Q46: Write a program to print the following pattern:
+/* *****
+   *****
+   *****
+   *****
+   *****
+*/
+int i, j;
+for (i = 1; i <= 5; i++) {
+for (j = 1; j <= 5; j++) {
+printf("*");
+}
+printf("\n");
+}
+return 0;
+}
+
